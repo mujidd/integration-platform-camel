7069de7930f589a6126fb70848d108d6302455f8
xlin33 <xlin33@ford.com>
2022-03-14 23:01:02 +0800

    feat:

--- src/main/java/com/demo/integrationplatform/dynamicrouterload/DynamicRoutesConfigurer.java
+++
@@ -1,7 +0,0 @@
-package com.demo.integrationplatform.dynamicrouterload;
-
-import org.apache.camel.CamelContext;
-
-public interface DynamicRoutesConfigurer {
-    void configureRoutes(CamelContext camelContext) throws Exception;
-}
--- src/main/java/com/demo/integrationplatform/springbean/MySpringBootRouter.java
+++ src/main/java/com/demo/integrationplatform/springbean/MySpringBootRouter.java
@@ -1,19 +1,39 @@
 package com.demo.integrationplatform.springbean;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.kafka.KafkaConstants;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MySpringBootRouter extends RouteBuilder {
-
+    final String saslJaasConfig = "RAW(org.apache.kafka.common.security.plain.PlainLoginModule required username=\"$ConnectionString\" password=\"Endpoint=sb://ivislevents.servicebus.chinacloudapi.cn/;SharedAccessKeyName=test;SharedAccessKey=ZiA2sJN2Q4TJ1JHAxBRUchjm+NLAZCi+i+xjMXUJHK8=;EntityPath=loginevents\";)";
+    final String brokers = "ivislevents.servicebus.chinacloudapi.cn:9093";
     @Override
     public void configure() {
-//        from("timer:hello?period=1000").routeId("hello")
-//                        .transform().method("myBean", "saySomething")
-//                        .filter(simple("${body} contains 'foo'"))
-//                        .to("log:foo")
-//                        .end()
-//                        .to("stream:out").onException(Exception.class);
+//        kafka(topic)
+//                .brokers(props.getProperty("bootstrap.servers"))
+//                .saslMechanism(props.getProperty("sasl.mechanism"))
+//                .securityProtocol(props.getProperty("security.protocol"))
+//                .sslEndpointAlgorithm(props.getProperty("ssl.endpoint.identification.algorithm"))
+//                .saslJaasConfig(props.getProperty("sasl.jaas.config")))
+        from("timer:hello?period=5000").routeId("camel")
+                .setBody(constant("Message from Camel"))          // Message to send
+                .setHeader(KafkaConstants.KEY, constant("Camel")) // Key of the message
+                .to("kafka:loginevents?"
+                        + "brokers=" + brokers
+                        + "&saslMechanism=PLAIN"
+                        + "&securityProtocol=SASL_SSL"
+                        + "&saslJaasConfig=" + saslJaasConfig);
+        from("kafka:loginevents?"
+                + "brokers=" + brokers
+                + "&saslMechanism=PLAIN"
+                + "&securityProtocol=SASL_SSL"
+                + "&saslJaasConfig=" + saslJaasConfig)
+                .log("Message received from Kafka : ${body}")
+                .log("    on the topic ${headers[kafka.TOPIC]}")
+                .log("    on the partition ${headers[kafka.PARTITION]}")
+                .log("    with the offset ${headers[kafka.OFFSET]}")
+                .log("    with the key ${headers[kafka.KEY]}");
     }
 
 }
