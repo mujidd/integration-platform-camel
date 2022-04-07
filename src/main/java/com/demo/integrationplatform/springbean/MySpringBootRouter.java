63caaabfe951dfd69351e71bae15e8d368a7f387
xlin33 <xlin33@ford.com>
2022-04-07 16:58:04 +0800

    feat(add kafka token ):

--- src/main/java/com/demo/integrationplatform/springbean/MySpringBootRouter.java
+++ src/main/java/com/demo/integrationplatform/springbean/MySpringBootRouter.java
@@ -2,6 +2,7 @@ package com.demo.integrationplatform.springbean;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.kafka.KafkaConstants;
+import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,6 +11,24 @@ public class MySpringBootRouter extends RouteBuilder {
     final String brokers = "ivislevents.servicebus.chinacloudapi.cn:9093";
     @Override
     public void configure() {
+        restConfiguration()
+                // Use the 'servlet' component.
+                // This tells Camel to create and use a Servlet to 'host' the RESTful API.
+                .component("servlet")
+                .enableCORS(false)
+                .host("localhost").port(8080)
+                // Allow Camel to try to marshal/unmarshal between Java objects and JSON
+                .bindingMode(RestBindingMode.auto);
+//        rest()
+//                .path("/v1/d/is-eventhub/loginStatus") // This makes the API available at http://host:port/$CONTEXT_ROOT/api
+//
+//                .consumes("application/json")
+//                .produces("application/json")
+//
+//                // HTTP: POST /rest
+//                .post()
+//                .to("direct:eventhub");
+
 //        from("timer:hello?period=5000").routeId("camel")
 //                .setBody(constant("Message from Camel"))          // Message to send
 //                .setHeader(KafkaConstants.KEY, constant("Camel")) // Key of the message
@@ -29,5 +48,4 @@ public class MySpringBootRouter extends RouteBuilder {
                 .log("    with the offset ${headers[kafka.OFFSET]}")
                 .log("    with the key ${headers[kafka.KEY]}");
     }
-
 }
